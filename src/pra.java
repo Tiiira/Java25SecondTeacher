3b596d379652f3e7b449ab9bc095938225f20ff0
Tira <cnn270.pc@gmail.com>
2025-04-28 14:05:50 +0800

    bingo

--- src/pra.java
+++ src/pra.java
@@ -1,5 +1,13 @@
 public class pra {
     public static void main(String[] args) throws Exception {
         System.out.println("啥都沒有~?");
+        int sum = 0;
+        int[] bingo={10, 24, 38, 41, 57, 68, 87, 94, 97};
+        for (int i = 0; i < bingo.length; i++) {
+            sum = sum + bingo[i];
+        }
+        System.out.println(sum);
+
+
     }
 }
